a319ee6efd06e0b4615a87d7cb4d5a758e66aec5
Myke Bates <contact@mykebates.com>
2020-12-08 01:37:21 -0600

    Listing Events, Companies/Sponsors
    Testing out Automapper concept. Several ways to approach the concept of mixed lists of things but wanted to kick the tires with AutoMapper - first go.

---
+++ SgfDevs/Dev/AutoMapperConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web.PublishedModels;
+using SgfDevs.ViewModels;
+
+namespace SgfDevs.Dev
+{
+    public static class AutoMapperConfig
+    {
+        public static IMapper Configure()
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<CommunityEvent, EventViewModel>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Location, opt => opt.MapFrom(src => src.City + ", " + src.State))
+                .ForMember(dest => dest.StartTime, opt => opt.MapFrom(src => src.StartTime))
+                .ForMember(dest => dest.Host, opt => opt.MapFrom(src => src.HostName));
+
+                cfg.CreateMap<Event, EventViewModel>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Location, opt => opt.MapFrom(src => "Springfield, MO"))
+                .ForMember(dest => dest.StartTime, opt => opt.MapFrom(src => src.Date))
+                .ForMember(dest => dest.Host, opt => opt.MapFrom(src => "Springfield Devs"))
+                .ForMember(dest => dest.IsDevNight, opt => opt.MapFrom(src => true));
+            });
+
+            var mapper = config.CreateMapper();
+
+            return mapper;
+        }
+    }
+}
---
+++ SgfDevs/ViewModels/EventViewModel.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web.PublishedModels;
+
+namespace SgfDevs.ViewModels
+{
+    public class EventViewModel : Profile
+    {
+        public string Name { get; set; }
+        public IPublishedContent Image { get; set; }
+        public string Location { get; set; }
+        public string Price { get; set; }
+        public string Host { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsDevNight { get; set; }
+    }
+}
