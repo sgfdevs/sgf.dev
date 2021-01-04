66db8026e628b0c45b27789e1420b75d8f392e49
Myke Bates <contact@mykebates.com>
2021-01-03 23:30:52 -0600

    Initial directory functionality progress

---
+++ SgfDevs/ViewModels/DirectoryResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SgfDevs.ViewModels
+{
+    public class DirectoryResult
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Image { get; set; }
+        public string Url { get; set; }
+    }
+}
