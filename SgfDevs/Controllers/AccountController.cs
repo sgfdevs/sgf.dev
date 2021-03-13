58947d662cd0d2ca8cf04fbc78a3866179e00e3a
Megan Blevins <mblevins@marlinnetwork.com>
2021-03-13 16:02:57 -0600

    emails sending... jankily

--- SgfDevs/Controllers/AccountController.cs
+++ SgfDevs/Controllers/AccountController.cs
@@ -6,6 +6,9 @@ using Umbraco.Web.Mvc;
 using System.Web.Mvc;
 using SgfDevs.Models;
 using SgfDevs.ViewModels;
+using System.Net.Mail;
+using SgfDevs.Dev;
+using Umbraco.Web.Composing;
 
 namespace SgfDevs.Controllers
 {
@@ -97,8 +100,22 @@ namespace SgfDevs.Controllers
                 //    //Save the member with the up[dated property value
                 memberService.Save(member);
                 ViewData["Saved"] = "saved";
+                var firstHalfEmail = "<!DOCTYPE HTML><html xmlns='http://www.w3.org/1999/xhtml' xmlns:v='urn:schemas-microsoft-com:vml' xmlns:o='urn:schemas-microsoft-com:office:office'><head><title>Reset Password</title> <!--[if !mso]><!-- --><meta http-equiv='X-UA-Compatible' content='IE=edge'> <!--<![endif]--><meta http-equiv='Content-Type' content='text/html; charset=UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1'><style type='text/css'>#outlook a{padding:0}body{margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%}table,td{padding:20px;border-collapse:collapse;mso-table-lspace:0pt;mso-table-rspace:0pt}img{border:0;height:auto;line-height:100%;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic}.h1{font-size:36px;font-weight:700;color:#153558;letter-spacing:0;line-height:43px;margin-bottom:0}p{font-family:'Open Sans',sans-serif;font-size:18px;line-height:1.666em;font-weight:400;color:#254151;display:block;margin:13px 0}.button{background:#619ECE;border-radius:25.5px;font-weight:700;font-size:14px;color:#fff!important;line-height:40px;padding:0 24px;border:0;height:40px;display:inline-flex;align-items:center;text-decoration:none;font-family:'Open Sans',sans-serif}</style><!--[if mso]> <xml> <o:OfficeDocumentSettings> <o:AllowPNG/> <o:PixelsPerInch>96</o:PixelsPerInch> </o:OfficeDocumentSettings> </xml> <![endif]--> <!--[if lte mso 11]><style type='text/css'>.outlook-group-fix{width:100% !important}</style><![endif]--><!--[if !mso]><!--><link href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap' rel='stylesheet' type='text/css'><style type='text/css'>@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap');</style><!--<![endif]--></head><body style='background-color:#E1E1E1;'><div style='display:none;font-size:1px;color:#ffffff;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;'> Need to reset your password? No biggie. Just click below to get started.</div><div style='background-color:#E1E1E1;'><!--[if mso | IE]><table align='center' border='0' cellpadding='0' cellspacing='0' class='' style='width:600px;' width='600' ><tr><td style='line-height:0px;font-size:0px;mso-line-height-rule:exactly;'> <![endif]--><div style='background:#ffffff;background-color:#ffffff;margin:0px auto;max-width:600px;'><table align='center' border='0' cellpadding='0' cellspacing='0' role='presentation' style='background:#ffffff;background-color:#ffffff;width:100%;'><tbody><tr><td style='direction:ltr;font-size:0px;padding:0px;text-align:center;'> <!--[if mso | IE]><table role='presentation' border='0' cellpadding='0' cellspacing='0'><tr><td class='' style='vertical-align:top;width:600px;' > <![endif]--><div class='mj-column-per-100 outlook-group-fix' style='font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;'><table border='0' cellpadding='0' cellspacing='0' role='presentation' width='100%'><tbody><tr><td style='vertical-align:top;padding:0px;'><table border='0' cellpadding='0' cellspacing='0' role='presentation' style='' width='100%'><tr><td align='center' style='font-size:0px;padding:0px;word-break:break-word;'><table border='0' cellpadding='0' cellspacing='0' role='presentation' style='border-collapse:collapse;border-spacing:0px;'><tbody><tr><td style='width:600px;'></td></tr><tr><td style='width:600px;'><p class='h1'>Reset your sgf.dev password</p><p>Need to reset your password? No biggie. Just click below to get started.</p></td></tr><tr><td style='width:600px;'> <a href='https://sgf.dev/reset-password?token=";
+                var secondHalfEmail = "' class='button'>Reset Your Password</a></td></tr></tbody></table></td></tr></table></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table> <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table> <![endif]--></div></body></html>";
+                var mm = new MailMessage
+                {
+                    Body = firstHalfEmail + member.GetValue("ResetPasswordToken") + secondHalfEmail,
+                    IsBodyHtml = true,
+                    Subject = "Reset SGF.Dev Password",
+                    From = new MailAddress("noreply@sgf.dev")
 
-                //    //Send user an email to reset password with token in it
+                };
+
+                mm.To.Add(new MailAddress(model.EmailAddress.ToString()));
+
+                new SmtpClient().Send(mm);
+
+
                 //    //EmailHelper email = new EmailHelper();
                 //    //email.SendResetPasswordEmail(findMember.Email, expiryTime.ToString("ddMMyyyyHHmmssFFFF"));
                 return CurrentUmbracoPage();
