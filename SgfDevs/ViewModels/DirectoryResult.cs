f8357e9fb62563a3dc99372ec6ba82d6632a825a
Levi Zitting <me@levizitting.com>
2024-03-10 16:22:46 -0500

    feat: add 2024 supporting member tag (#55)
    * feat: add supporting member tag
    
    * feat: update FE to use new tag

--- SgfDevs/ViewModels/DirectoryResult.cs
+++ SgfDevs/ViewModels/DirectoryResult.cs
@@ -6,6 +6,6 @@ public class DirectoryResult
     public string Location { get; set; }
     public string Image { get; set; }
     public string Url { get; set; }
-
-    public bool FoundingMember { get; set; }
+    public bool IsFoundingMember { get; set; }
+    public bool Is2024SupportingMember { get; set; }
 }
