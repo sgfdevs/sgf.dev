f42e2b66c9d39564eee85efaacb48402f5fa82b9
Josh Shavers <shaversj@gmail.com>
2024-03-23 08:19:55 -0500

    Fix export default to match project format

--- sgfdevs-frontend/src/components/RichText.tsx
+++ sgfdevs-frontend/src/components/RichText.tsx
@@ -1,12 +1,10 @@
-const RichText = ({ htmlString }: { htmlString: string }) => {
+export default function RichText({ htmlString }: { htmlString: string }) {
   return (
     <div
       className={
-        'prose-h1:text-[2rem] sm: prose-h1:text-[#254151] prose-h1:font-black prose-h1:my-[0.67em] prose-p:my-[1em] mx-auto max-w-[1028px] px-[30px] font-sans'
+        'sm: mx-auto max-w-[1028px] px-[30px] font-sans prose-h1:my-[0.67em] prose-h1:text-[2rem] prose-h1:font-black prose-h1:text-[#254151] prose-p:my-[1em]'
       }
       dangerouslySetInnerHTML={{ __html: htmlString }}
     />
   );
-};
-
-export default RichText;
+}
