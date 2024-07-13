4c640d3550b4a93d33b873afef18e2794b1ad5fa
Levi Zitting <me@levizitting.com>
2024-07-13 11:06:42 -0500

    fix: typo

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,11 +2,12 @@
 
 # Get the directory the script resides in, navigate their, and load env variables
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo $__dir
 cd $__dir
 export $(cat .env | xargs)
 
 cd ..
 
-restic -r $RESTIC_REPOSITORY_PREFIX/sgf.dev backup init
+restic -r $RESTIC_REPOSITORY_PREFIX/sgf.dev init
 
 (crontab -l 2>/dev/null; echo "0 6 * * *  /home/ubuntu/sgf/dev/scripts/backup.sh") | crontab -
